97895f7ece09b4bffd1ecc7366a8409638d35cf1
sdpb21 <jytprf@gmail.com>
2025-04-14 01:36:43 -0300

    Code to test the servo completed, ready to upload to the ESP32

--- servoTest/src/main.cpp
+++ servoTest/src/main.cpp
@@ -7,9 +7,24 @@ Servo servo1;
 
 void setup() {
 
-  servo1.attach(servoDataPin);
+  servo1.attach(servoDataPin);  // attach or "link" the pin to the servo
+
+  servo1.write(0);              // set the servo angle to 0 degrees
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  delay(2000);      // 2 seconds delay
+  servo1.write(90); // sets the servo angle to 90 degrees
+
+  delay(2000);      // 2 seconds delay
+  servo1.write(180);// sets the servo angle to 180 degrees
+
+  delay(2000);      // 2 seconds delay
+  servo1.write(90); // sets the servo angle to 90 degrees
+
+  delay(2000);      // 2 seconds delay
+  servo1.write(0);  // sets the servo angle to 0 degrees
+
 }
