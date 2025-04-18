8295a67e7ba1706400d4e7e2df5651655cb871b5
sdpb21 <jytprf@gmail.com>
2025-04-18 18:52:22 -0300

    Soft access point variables for ssid and password created

--- servoInterruptorOnOff/src/main.cpp
+++ servoInterruptorOnOff/src/main.cpp
@@ -2,6 +2,9 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
+const char* ssid="******";    // soft access point name
+const char* password="****";  // soft access point password
+
 void setup() {
   // put your setup code here, to run once:
 
