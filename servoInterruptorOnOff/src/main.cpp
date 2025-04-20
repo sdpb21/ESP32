262693800efeff78f698055d542448501f9c52fc
sdpb21 <jytprf@gmail.com>
2025-04-20 19:29:40 -0300

    WebServer object created

--- servoInterruptorOnOff/src/main.cpp
+++ servoInterruptorOnOff/src/main.cpp
@@ -9,6 +9,8 @@ IPAddress ip(192, 168, 4, 22);      // local IP of soft access point
 IPAddress gateway(192, 168, 4, 9);  // soft access point gateway
 IPAddress subnet(255, 255, 255, 0); // soft access point subnet mask
 
+WebServer server(80);   // WebServer object created
+
 void setup() {
   // put your setup code here, to run once:
 
