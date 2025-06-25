69e6226993be7b3710326c9277e875c0cda03499
sdpb21 <jytprf@gmail.com>
2025-06-25 10:59:03 -0300

    Servo test code with espressif libraries, some libraries included

--- servoTest/src/main.cpp
+++ servoTest/src/main.cpp
@@ -1,20 +1,30 @@
+/*
 #include <Arduino.h>
 #include <ESP32Servo.h>
+*/
+
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "freertos/timers.h"
+#include "driver/ledc.h"
 
 #define servoDataPin 26
 
-Servo servo1;
+//Servo servo1;
 
 void setup() {
-
+/*
   servo1.attach(servoDataPin);  // attach or "link" the pin to the servo
 
   servo1.write(0);              // set the servo angle to 0 degrees
-
+*/
 }
 
 void loop() {
-
+/*
   delay(500);      //  a half second delay
   servo1.write(90); // sets the servo angle to 90 degrees
 
@@ -26,5 +36,5 @@ void loop() {
 
   delay(500);      // a half second delay
   servo1.write(0);  // sets the servo angle to 0 degrees
-
+*/
 }
