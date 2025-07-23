ab23fdd9db44fa0fedefe8548c9006b9551d3024
sdpb21 <jytprf@gmail.com>
2025-07-23 11:50:25 -0300

    PWM channels configured

--- servoTest2/main/pwm_youtube_example.c
+++ servoTest2/main/pwm_youtube_example.c
@@ -39,5 +39,31 @@ esp_err_t set_timer(void){
 }
 
 esp_err_t set_pwm(void){
-    //
+    
+    ledc_channel_config_t channelConfigR = {0};
+    ledc_channel_config_t channelConfigG = {0};
+    ledc_channel_config_t channelConfigB = {0};
+
+    channelConfigR.gpio_num = 33;
+    channelConfigR.speed_mode = LEDC_HIGH_SPEED_MODE;
+    channelConfigR.channel = LEDC_CHANNEL_0;
+    channelConfigR.intr_type = LEDC_INTR_DISABLE;
+    channelConfigR.timer_sel = LEDC_TIMER_0;
+    channelConfigR.duty = 0;
+
+    channelConfigG.gpio_num = 25;
+    channelConfigG.speed_mode = LEDC_HIGH_SPEED_MODE;
+    channelConfigG.channel = LEDC_CHANNEL_1;
+    channelConfigG.intr_type = LEDC_INTR_DISABLE;
+    channelConfigG.timer_sel = LEDC_TIMER_0;
+    channelConfigG.duty = 0;
+
+    channelConfigB.gpio_num = 26;
+    channelConfigB.speed_mode = LEDC_HIGH_SPEED_MODE;
+    channelConfigB.channel = LEDC_CHANNEL_2;
+    channelConfigB.intr_type = LEDC_INTR_DISABLE;
+    channelConfigB.timer_sel = LEDC_TIMER_0;
+    channelConfigB.duty = 0;
+
+    return ESP_OK;
 }
