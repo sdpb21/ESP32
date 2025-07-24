2f365e102a9c3e09cbe240e78a0ff2176fca7a20
sdpb21 <jytprf@gmail.com>
2025-07-24 12:01:52 -0300

    set_pwm_duty function completed

--- servoTest2/main/pwm_youtube_example.c
+++ servoTest2/main/pwm_youtube_example.c
@@ -10,20 +10,43 @@
 uint8_t led_level = 0;
 static const char *tag = "Main";
 TimerHandle_t xTimers;
-int interval = 500;
+int interval = 50;
 int timerId = 1;
+uint32_t dutyR = 0;
+uint32_t dutyG = 300;
+uint32_t dutyB = 600;
 
 esp_err_t init_led(void);
 esp_err_t blink_led(void);
 esp_err_t set_timer(void);
 esp_err_t set_pwm(void);
+esp_err_t set_pwm_duty(void);
 
 void vTimerCallback(TimerHandle_t pxTimer){
-    //
+    
+    dutyR += 10;
+    if( dutyR > 1023 )
+        dutyR = 0;
+    
+    dutyG += 10;
+    if( dutyG > 1023 )
+        dutyG = 0;
+    
+    dutyB += 10;
+    if( dutyB > 1023 )
+        dutyB = 0;
+    
+    blink_led();
+    set_pwm_duty();
+
 }
 
 void app_main(void){
-    //
+
+    init_led();
+    set_pwm();
+    set_timer();
+
 }
 
 esp_err_t init_led(void){
@@ -87,4 +110,18 @@ esp_err_t set_pwm(void){
     ledc_timer_config( &timerConfig );
 
     return ESP_OK;
+}
+
+esp_err_t set_pwm_duty(void){
+
+    ledc_set_duty( LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, dutyR);
+    ledc_set_duty( LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, dutyG);
+    ledc_set_duty( LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, dutyB);
+
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2);
+
+    return ESP_OK;
+
 }
