470a2dc66ae3cdeb4352b4105e7d697034d70434
sdpb21 <jytprf@gmail.com>
2025-07-28 12:32:00 -0300

    set_timer function defined

--- timer_youtube_example/main/main.c
+++ timer_youtube_example/main/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#define led1 2
+
+uint8_t led_level = 0;
+
+esp_err_t init_led(void);
+esp_err_t blink_led(void);
+esp_err_t set_timer(void);
 
 void app_main(void)
 {
+    init_led();
+
+}
+
+esp_err_t init_led(void){
+
+    gpio_reset_pin(led1);
+    gpio_set_direction( led1, GPIO_MODE_OUTPUT);
+
+    return ESP_OK;
+
+}
+
+esp_err_t blink_led(void){
+
+    led_level = !led_level;
+    gpio_set_level( led1, led_level);
+
+    return ESP_OK;
 
 }
